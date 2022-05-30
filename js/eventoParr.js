a38277f6b3b9b2412072be4e06036990445d3fb3
Constantin Vlad <67373492+ConstanHin@users.noreply.github.com>
2022-05-30 13:26:28 +0200

    finalizado 13

--- js/eventoParr.js
+++ js/eventoParr.js
@@ -1,1 +1,29 @@
-// Constantin Vlad
+// by Constantin Vlad
+
+// Metodo a ejecutar al hacer click en el párrafo
+const onParagrafoClick = function () {
+
+    // Mostrar display
+    display.style.opacity = 1;
+    // Disminuir opacidad con temporizador
+    setTimeout(() => display.style.opacity = 0, 1000);
+
+    // Comprobar si es un párrafo de la segunda tabla
+    document.querySelector("#tabla1 p").closest("#tabla2")
+    if (this.closest("#tabla2")) {
+        display.innerText = "Se pulsó un párrafo de la segunda tabla"; 
+    } else {
+        display.innerText = "Se presiono un párrafo del documento"; 
+    }
+}
+
+// Obtenemos todos los párrafos de la página
+const elementos = document.querySelectorAll("p");
+
+// Setear la acción a realizar al hacer click en el párrafo
+for (let index = 0; index < elementos.length; index++) {
+    elementos.item(index).onclick = onParagrafoClick;
+}
+
+// Obtenemos el display para mostrar el mensaje
+const display = document.getElementById("display");
